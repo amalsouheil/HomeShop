9cee81c5496f0a83a440c5fd16ea0230adc897dc
Amal <amalsouheil@gmail.com>
2019-01-25 14:33:35 +0100

    création des deux classes Bill et Customer et d'un dossier test

---
+++ src/com/ocr/amal/Cutomer.java
@@ -0,0 +1,16 @@
+package com.ocr.amal;
+
+public class Cutomer {
+
+String fullname;
+
+    public String getAdress() {
+        return adress;
+    }
+
+    String adress;
+
+    public String getFullname() {
+        return fullname;
+    }
+}
